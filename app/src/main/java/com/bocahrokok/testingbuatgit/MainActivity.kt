1b93b8a96c3862e241f592ab1ac5f786d1664c44
bocahrokok <indieunistudy@gmail.com>
2020-03-03 19:07:26 +0700

    ini nambahin function

---
+++ app/src/main/java/com/bocahrokok/testingbuatgit/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.bocahrokok.testingbuatgit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    private fun halo(){
+        //isinya buat komen
+    }
+}
